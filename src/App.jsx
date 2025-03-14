6c1c28bd724cccb870f3a3779e9d22cae7761ee8
Julia Selska <juliapselska@gmail.com>
2025-03-14 17:28:12 +0100

    homework 4

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+
+
+const App = () => {
+
+  const [pictures, setPictures] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchPictures() {
+      const response = await axios.get("https://api.unsplash.com/search/photos?query=nature&per_page=10&client_id=hRxXyIs8LjiWw29USD0xMbDQ0ofvHUO75pu1Bv3PQ4o");
+
+      setPictures(response.data.hits);
+
+    }
+    fetchPictures();
+  }, []);
+
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+
+  )
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
